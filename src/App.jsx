dca244216a496068cd80561e808f69894ed86313
Marcial99 <61446118+Marcial99@users.noreply.github.com>
2022-08-04 13:30:00 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Filtros from "./components/Filtros";
 import Modal from "./components/Modal";
 import ListadoGastos from "./components/ListadoGastos";
